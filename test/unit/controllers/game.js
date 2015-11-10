8d96d2a26d20fa89955dd43de6c4283d6835184b
Nazarov Andrey <nazandrbox@yandex.ru>
2015-11-10 22:01:26 +0300

    Cleaning game ctrl test.

--- test/unit/controllers/game.js
+++ test/unit/controllers/game.js
@@ -39,30 +39,5 @@ describe('Controller: GameCtrl', function () {
   
   it('should have turn rules', inject(function (rules) {
     expect(rules.getTurnRules()).toBeDefined();
-  }));
-  
-  it('should not to change shape if this is not empty already', inject(function () {
-    //this is for e2e testing
-    /*
-    var field = Field_ctrl.getField(),
-      col = 0,
-      row = 0,
-      old_shape = field[row][col].shape,
-      curr_player_shape_arr = scope.getCurrPlayerShapeArr();
-    scope.turn(row,col);
-    field = Field_ctrl.getField();
-    var new_shape = field[row][col].shape;
-    expect(new_shape).not.toBe(old_shape);
-    expect(curr_player_shape_arr).toContain(new_shape);
-    
-    curr_player_shape_arr = scope.getCurrPlayerShapeArr();
-    old_shape = new_shape;
-    scope.turn(row,col);
-    field = Field_ctrl.getField();
-    new_shape = field[row][col].shape;
-    expect(new_shape).toBe(old_shape);
-    expect(curr_player_shape_arr).toContain(new_shape);
-    */
-  }));
-  
+  }));  
 });

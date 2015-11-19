3a393dd83186672be185d2b5dbb0b7be2db4e560
Nazarov Andrey <nazandrbox@yandex.ru>
2015-11-19 22:33:20 +0300

    Clearing phantomJS hack

--- test/e2e/game/shape_change.js
+++ test/e2e/game/shape_change.js
@@ -1,15 +1,7 @@
 describe('game shape change', function() {
-  var p = require('path');
-  console.log(p.normalize('../hacks/phantom_click.js'));
-  var phantomClick = require('../hacks/phantom_click.js'); 
-  
-  
-  
-
   beforeEach(function(){
     browser.get('/#/game');
     browser.waitForAngular();
-    phantomClick.init();
   });
   
   it('should change in first time and not in second', function() { 

a1c6dfe246457ba74b7d04df0daf23987cc7f45f
unknown <Nazarov.Andrey@itm-39-10.corp.wildberries.ru>
2017-07-13 17:51:29 +0300

    Fix useminPrepare grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,9 @@ module.exports = function (grunt) {
   // Load grunt tasks automatically
   //require('load-grunt-tasks')(grunt);
   
-  require('jit-grunt')(grunt);
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);

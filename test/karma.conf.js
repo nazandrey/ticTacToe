2f99b1aee7b0f73cb039321f5924ef13f028253d
unknown <nazarov.andrey@itm-39-10.corp.wildberries.ru>
2017-07-14 11:50:52 +0300

    Allow phantomjs handle "Function.prototype.bind"
    To avoid "Failed to instantiate module ng due to:" at unit test.
    Added phantomjs-polyfill lib for this.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
+      "./node_modules/phantomjs-polyfill/bind-polyfill.js",
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
       "test/unit/**/*.js",

641fff802dd0ac3bfa885ef166a24e035952a6a2
nazandrey <nazandrbox@yandex.ru>
2017-07-17 12:10:19 +0300

    Add playerRuleList test

---
+++ test/unit/services/playerRuleList.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Service: playerRuleList', function () {
+
+  // load the service's module
+  beforeEach(module('ticTacToeApp'));
+
+  // instantiate service
+  var playerRuleList,
+    currPlayerRuleList;
+  beforeEach(inject(function (_playerRuleList_) {
+    playerRuleList = _playerRuleList_;
+    currPlayerRuleList = playerRuleList.get();
+  }));
+
+  it('should give player rule list', function () {
+    expect(currPlayerRuleList).toBeObject();
+  });
+
+  it('should have shapeArr in given field rule list', function () {
+    expect(currPlayerRuleList.getShapeArr()).toBeArray();
+  });
+});

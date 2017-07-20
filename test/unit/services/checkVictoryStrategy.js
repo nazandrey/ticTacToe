dbb24bf9490cdf5a6ebc667892c97d6a89db5aec
nazandrey <nazandrbox@yandex.ru>
2017-07-20 16:30:33 +0300

    Add test checking victory by default strategy

--- test/unit/services/checkVictoryStrategy.js
+++ test/unit/services/checkVictoryStrategy.js
@@ -13,4 +13,32 @@ describe('Service: checkVictoryStrategy', function () {
     expect(checkVictoryStrategy.getCurr).toBeFunction();
     expect(checkVictoryStrategy.getCurr()).toBeFunction();
   });
+
+  describe('(default strategy, using information about field and players)', function () {
+    var currCheckVictoryStrategy,
+      player1,
+      player2,
+      playerList;
+
+    beforeEach(inject(function (_playerFactory_) {
+      playerFactory = _playerFactory_;
+      currCheckVictoryStrategy = checkVictoryStrategy.getCurr(),
+      player1 = playerFactory.createPlayer(),
+      player2 = playerFactory.createPlayer(),
+      playerList = [player1, player2],
+      oo = player1.getMainShape(),
+      xx = player2.getMainShape();
+    }));
+
+    it('should be player1 victory', function (playerFactory) {
+      fieldModelMock = [
+        [oo,oo,oo,oo,oo],
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','',''],
+      ];
+      expect(currCheckVictoryStrategy(fieldModelMock, playerList)).toBe("player1");
+    });
+  });
 });

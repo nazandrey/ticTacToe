601b4bfe8726f6f6a91da0c342ec3107e614a35f
nazandrey <nazandrbox@yandex.ru>
2017-07-21 16:42:23 +0300

    Add createPlayer in playerFactory

--- app/scripts/services/playerFactory.js
+++ app/scripts/services/playerFactory.js
@@ -8,9 +8,23 @@
  * Factory in the ticTacToeApp.
  */
 angular.module('ticTacToeApp')
-  .factory('playerFactory', function () {
+  .factory('playerFactory', function (playerRuleList) {
+    var playerShapeArrList = playerRuleList.get().playerShapeArr;
+
+    function createPlayer () {
+      var player = null;
+      if (playerShapeArrList && playerShapeArrList.length) {
+        var playerShapeArr = playerShapeArrList.shift();
+
+        player = {
+          shapeArr: playerShapeArr,
+          getMainShape: function () { return this.shapeArr[0]; }
+        }
+      }
+      return player;
+    }
 
     return {
-
+      createPlayer: createPlayer
     };
   });

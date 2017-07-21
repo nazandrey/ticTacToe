c5605088a2c43a244cce86acb287b37f8b5178ab
nazandrey <nazandrbox@yandex.ru>
2017-07-21 12:20:39 +0300

    Add playerFactory to project

---
+++ app/scripts/services/playerFactory.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ticTacToeApp.playerFactory
+ * @description
+ * # playerFactory
+ * Factory in the ticTacToeApp.
+ */
+angular.module('ticTacToeApp')
+  .factory('playerFactory', function () {
+
+    return {
+
+    };
+  });
---
+++ test/unit/services/playerFactory.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Service: playerFactory', function () {
+
+  beforeEach(module('ticTacToeApp'));
+
+  var playerFactory;
+  beforeEach(inject(function (_playerFactory_) {
+    playerFactory = _playerFactory_;
+  }));
+
+  it('should do something', function () {
+
+  });
+});

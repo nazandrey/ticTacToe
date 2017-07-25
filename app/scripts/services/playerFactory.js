3184fd689c40e3e84d11ef1fc548b54c39705f1f
nazandrey <nazandrbox@yandex.ru>
2017-07-25 18:40:37 +0300

    Add createPlayer in playerFactory

--- app/scripts/services/playerFactory.js
+++ app/scripts/services/playerFactory.js
@@ -8,9 +8,26 @@
  * Factory in the ticTacToeApp.
  */
 angular.module('ticTacToeApp')
-  .factory('playerFactory', function () {
+  .factory('playerFactory', function (playerRuleList) {
+    var playerCount = 0,
+      ruleList = playerRuleList.get(),
+      playerShapeArrList = ruleList.playerShapeArr;
+
+    function createPlayer () {
+      var player = null;
+      if (playerShapeArrList && playerShapeArrList.length) {
+        var playerShapeArr = playerShapeArrList.shift();
+
+        player = {
+          name: "player" + (++playerCount),
+          shapeArr: playerShapeArr,
+          getMainShape: function () { return this.shapeArr[0]; }
+        }
+      }
+      return player;
+    }
 
     return {
-
+      createPlayer: createPlayer
     };
   });
--- test/unit/services/playerFactory.js
+++ test/unit/services/playerFactory.js
@@ -4,12 +4,38 @@ describe('Service: playerFactory', function () {
 
   beforeEach(module('ticTacToeApp'));
 
+  beforeEach(module(function ($provide) {
+    $provide.factory('playerRuleList', function(){
+      function get () {
+        return {
+          playerShapeArr: ['circle', 'cross']
+        }
+      }
+
+      return {
+        get: get
+      };
+    });
+  }));
+
   var playerFactory;
   beforeEach(inject(function (_playerFactory_) {
     playerFactory = _playerFactory_;
   }));
 
-  it('should do something', function () {
+  it('should create player', function () {
+    expect(playerFactory.createPlayer).toBeFunction();
+    expect(playerFactory.createPlayer()).toBeObject();
+  });
 
+  it('should create 2 players', function () {
+    playerFactory.createPlayer();
+    expect(playerFactory.createPlayer()).toBeObject();
+  });
+
+  it('should create 2 players but not more', function () {
+    playerFactory.createPlayer();
+    playerFactory.createPlayer();
+    expect(playerFactory.createPlayer()).toBeNull();
   });
 });

0b6b4154cbcf2b28d7a8f2f8bc974f462021ea2f
nazandrey <nazandrbox@yandex.ru>
2017-07-25 18:41:06 +0300

    Add simpleCheckVictory template

---
+++ app/scripts/services/simpleCheckVictory.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ticTacToeApp.simpleCheckVictory
+ * @description
+ * # simpleCheckVictory
+ * Factory in the ticTacToeApp.
+ */
+angular.module('ticTacToeApp')
+  .factory('simpleCheckVictory', function () {
+
+    return {
+
+    };
+  });
---
+++ test/unit/services/simpleCheckVictory.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Service: simpleCheckVictory', function () {
+
+  beforeEach(module('ticTacToeApp'));
+
+  var simpleCheckVictory;
+  beforeEach(inject(function (_simpleCheckVictory_) {
+    simpleCheckVictory = _simpleCheckVictory_;
+  }));
+
+  it('should do something', function () {
+
+  });
+});

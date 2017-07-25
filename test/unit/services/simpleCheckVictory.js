96c20c5ce4ee4181e922bf5bb7a2629ed31c1288
nazandrey <nazandrbox@yandex.ru>
2017-07-25 18:42:00 +0300

    Move checkVictoryStrategy tests to simpleCheckVictory

--- test/unit/services/simpleCheckVictory.js
+++ test/unit/services/simpleCheckVictory.js
@@ -9,7 +9,80 @@ describe('Service: simpleCheckVictory', function () {
     simpleCheckVictory = _simpleCheckVictory_;
   }));
 
-  it('should do something', function () {
+  var playerFactory,
+    simpleCheckVictory,
+    player1,
+    player2,
+    playerList,
+    oo,
+    xx;
 
+  beforeEach(inject(function (_playerFactory_) {
+    playerFactory = _playerFactory_;
+    simpleCheckVictory = simpleCheckVictory,
+    player1 = playerFactory.createPlayer(),
+    player2 = playerFactory.createPlayer(),
+    playerList = [player1, player2];
+  }));
+
+  beforeEach(function () {
+    oo = player1.getMainShape();
+    xx = player2.getMainShape();
+  });
+
+  it('should be player1 victory (horizontal)', function () {
+    var fieldModelMock = [
+      [oo,oo,oo,oo,oo],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','','']
+    ];
+    expect(simpleCheckVictory(fieldModelMock, playerList)).toBe("player1");
+  });
+
+  it('should be not victory (horizontal)', function () {
+    var fieldModelMock = [
+      [oo,xx,oo,oo,oo],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','','']
+    ];
+    expect(simpleCheckVictory(fieldModelMock, playerList)).toBe("");
+  });
+
+  it('should be player1 victory (vertical)', function () {
+    var fieldModelMock = [
+      ['',oo,'','',''],
+      ['',oo,'','',''],
+      ['',oo,'','',''],
+      ['',oo,'','',''],
+      ['',oo,'','','']
+    ];
+    expect(simpleCheckVictory(fieldModelMock, playerList)).toBe("player1");
+  });
+
+  it('should be player2 victory (diagonal)', function () {
+    var fieldModelMock = [
+      [xx,'','','',''],
+      ['',xx,'','',''],
+      ['','',xx,'',''],
+      ['','','',xx,''],
+      ['','','','',xx]
+    ];
+    expect(simpleCheckVictory(fieldModelMock, playerList)).toBe("player2");
+  });
+
+  it('should be player2 victory (reverse diagonal)', function () {
+    var fieldModelMock = [
+      ['','','','','',''],
+      ['','','','','',xx],
+      ['','','','',xx,''],
+      ['','','',xx,'',''],
+      ['','',xx,'','',''],
+      ['',xx,'','','','']
+    ];
+    expect(simpleCheckVictory(fieldModelMock, playerList)).toBe("player2");
   });
 });
